9c25aeab80df8f41e288d2b7355a504745647c98
Dowsley <joao.dowsley850@gmail.com>
2021-11-18 23:13:45 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowsParens: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: true,
+}
+
+module.exports = options
